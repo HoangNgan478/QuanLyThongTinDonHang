f4c0a02546c4ffd71229264ac1790f2a695ae9a9
Hoang Ngan <63082185+HoangNgan478@users.noreply.github.com>
2026-06-02 23:46:14 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const ADMIN_PASSWORD = "12346"; 
-const scriptURL = 'const scriptURL = 'https://script.google.com/macros/s/AKfycbwnZc6Q95DJvRpy-udroL0ldYaBWYKSjqu2HW0aqwwY9wkIkAKTlWkI7y7vxZC3Fhy_/exec';
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwnZc6Q95DJvRpy-udroL0ldYaBWYKSjqu2HW0aqwwY9wkIkAKTlWkI7y7vxZC3Fhy_/exec';
 const allFields = ['hoTen', 'sanPham', 'kichThuoc', 'soLuong', 'donGia', 'ghiChu', 'nguoi', 'ngayNhap', 'ngay', 'tinhTrang', 'thanhToan', 'daTra'];
 
 let isLockSync = false; 
